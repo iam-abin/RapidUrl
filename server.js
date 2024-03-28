97f57d4f56289b3952fecef139a1d333dbe48526
Abin <iamabin1100@gmail.com>
2024-03-28 07:36:32 +0530

    gonna start

---
+++ server.js
@@ -0,0 +1,10 @@
+import express from "express";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+	console.log(`listening on port ${PORT}`);
+});
